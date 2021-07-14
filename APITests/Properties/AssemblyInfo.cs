4d8b864f39ddfefa7b070dfc748a8b6bf01af914
VictoriaBadenko <Victoria.badenko.vb@gmail.com>
2021-07-14 22:05:53 +0300

    Add project files.

---
+++ APITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00f41570-1815-4005-a9e1-b2106b4db383")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APITests/Regression Tests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using APIDemo;
+
+namespace APITests
+{
+    [TestClass]
+    public class RegressionTests
+    {
+        [TestMethod]
+        public void VerifyListOfUsers()
+        {
+            var demo = new Demo<ListOfUsersDTO>();
+            var user = demo.GetUsers("api/users?page=2");
+            Assert.AreEqual(2, user.Page);
+            Assert.AreEqual("Michael", user.Data[0].first_name);
+        }
+
+        [TestMethod]
+        public void CreateNewUser()
+        {
+            string payload = @"{
+                                ""name"": ""Mike"",
+                                ""job"": ""Team leader""
+                               }";
+            var demo = new Demo<CreateUserDTO>();
+            var user = demo.CreateUser("api/users", payload);
+            
+
+            Assert.AreEqual("Mike", user.Name);
+            Assert.AreEqual("Team leader", user.Job);
+        }
+    }
+}
+
